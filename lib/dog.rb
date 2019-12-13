120e05118bf3c836d9585de78d4a2062e6b99b3a
wolfene2001 <wolfene2001@yahoo.com>
2019-12-13 16:44:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,7 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
-  def name=(name)
-    @name = name
-  end
-  def name
-    @name
-  end
+  
 end
