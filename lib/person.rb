f6f8feecf46351cca0bf3e73f14818ecf8917457
wolfene2001 <wolfene2001@yahoo.com>
2019-12-13 16:46:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,10 +2,10 @@ class Person
   def initialize(name)
     @name = name
   end
-  def name=(name)
-    @name = name
-  end
-  def name
-    @name
-  end
+#  def name=(name)
+#    @name = name
+#  end
+#  def name
+#    @name
+#  end
 end
